582826da77c614432ec08202a211962d48c45aec
endrial <endrialmp@gmail.com>
2024-12-28 01:44:09 +0700

    first commit

---
+++ e2e/Liking_Restaurants.test.js
@@ -0,0 +1,40 @@
+// eslint-disable-next-line no-unused-vars, no-undef
+const { I } = inject();
+
+// eslint-disable-next-line no-undef
+Feature("Restaurant Favorites");
+
+// eslint-disable-next-line no-undef
+Before(({ I }) => {
+  I.amOnPage("/");
+});
+
+// eslint-disable-next-line no-undef
+Scenario("Liking a restaurant", async ({ I }) => {
+  I.waitForElement(".resto-item_description", 5);
+  I.seeElement(".resto-item_description");
+  I.click(".resto-item > a ");
+
+  I.waitForElement("#likeButton", 2);
+  I.seeElement("#likeButton");
+  I.click("#likeButton");
+
+  I.amOnPage("/#/favorite");
+  I.waitForElement(".resto-item_name", 10);
+  I.seeElement(".resto-item_name");
+});
+
+// eslint-disable-next-line no-undef
+Scenario("Unliking a restaurant", async ({ I }) => {
+  I.waitForElement(".resto-item_description", 5);
+  I.seeElement(".resto-item_description");
+  I.click(".resto-item > a ");
+
+  I.waitForElement("#likeButton", 2);
+  I.seeElement("#likeButton");
+  I.click("#likeButton");
+
+  I.amOnPage("/#/favorite");
+  I.waitForElement(".resto-item_name", 10);
+  I.seeElement(".resto-item_name");
+});
